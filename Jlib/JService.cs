1f3ffc8ba9bd1b23f2a1fab9c26193d6a38c51ed
Joseph Wulf <JSW@JSWulf.com>
2018-07-17 13:28:07 -0700

    Class creation

--- Jlib/JService.cs
+++ Jlib/JService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Jlib
 {
-    public class FileIO
+    class JService
     {
     }
 }
--- JlibTests/Properties/AssemblyInfo.cs
+++ JlibTests/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jlib")]
+[assembly: AssemblyTitle("JlibTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jlib")]
+[assembly: AssemblyProduct("JlibTests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31373d00-2698-4766-b36b-f3b5f3204f0a")]
+[assembly: Guid("61a97513-5517-40ab-a05c-cad08cc6f406")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
